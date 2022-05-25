c85a0d640b7ea16ac3a5e0af0d598fb898b4c3ce
Luis Bringas <acevedo.bringas.luis.esteban@gmail.com>
2022-05-24 19:10:51 -0500

    Procesar el mensaje a cifrar

--- ProcesarMensaje.py
+++ ProcesarMensaje.py
@@ -4,6 +4,7 @@ def ProcesamientoMensaje (mensaje):
     Mensaje = mensaje.replace(" ","").upper()
     print(Mensaje)
 
+    #Eliminamos las letras con acentos, cambiamos a "v" por "u" y pasamos todo a mayusculas
     Acentos = (
         ("á", "a"),
         ("é", "e"),
@@ -22,23 +23,23 @@ def ProcesamientoMensaje (mensaje):
     ListaMensaje = list(Mensaje)
     longitud = len(ListaMensaje)
 
+    #Si el mensaje solo es 1 letra se le agrega una "x" al final
     if longitud == 1:
         ListaMensaje.append("X")
 
-    for k in ListaMensaje: #Comprobamos si hay letras repetidas, si las hay se inserta una X entre las letras repetidas
+    #Comprobamos si hay letras repetidas, si las hay se inserta una X entre las letras repetidas
+    for k in ListaMensaje: 
         if ListaMensaje[p] == ListaMensaje [q]:
             ListaMensaje.insert(q,"X")
         p += 1
         q += 1
         if   q >= longitud: 
-            # print(ListaMensaje)
             break
     
     longitud = len(ListaMensaje) #Actualizamos la longitud de la lista
 
     if longitud % 2 != 0: #Comprobamos si el numero de letras en le mensaje es par, si no lo es se agrega una x al final
         ListaMensaje.append("X")
-        # print(ListaMensaje)
 
     return ListaMensaje
 
