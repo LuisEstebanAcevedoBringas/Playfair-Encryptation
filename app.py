b9f807951ca82ca10f7c3656b735e3fbf4c2a938
Luis Bringas <acevedo.bringas.luis.esteban@gmail.com>
2022-06-11 14:51:45 -0500

    Flask app

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, flash
+from DecifradoPlayfair import DecifradoPlayfair
+from CifradoPlayfair import CifrarPlayfair
+
+app = Flask(__name__)
+
+app.secret_key = "Password"
+
+@app.route("/playfair")
+
+def index():
+    flash(" ")
+    return render_template("index.html")
+
+@app.route("/cifrar", methods = ["POST", "GET"])
+def cifrar():
+    Mensaje = request.form['Mensaje_Cifrar']
+    Palabra = request.form["Palabra_Cifrar"]
+    MC = CifrarPlayfair(Mensaje, Palabra)
+    flash(MC)
+    return render_template("index.html")
+
+@app.route("/decifrar", methods = ["POST", "GET"])
+def decifrar():
+    Mensaje_D = request.form['Mensaje_Decifrar']
+    Palabra_D = request.form["Palabra_Decifrar"]
+    MD = DecifradoPlayfair(Mensaje_D, Palabra_D)
+    flash(MD)
+    return render_template("index.html")
