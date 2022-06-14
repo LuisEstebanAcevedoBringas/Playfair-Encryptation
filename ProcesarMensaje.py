2cf3b2df9734efb96ac1d1fde51327de71b969f2
Luis Bringas <acevedo.bringas.luis.esteban@gmail.com>
2022-06-13 20:47:41 -0500

    Remove special characters and numbers.

--- ProcesarMensaje.py
+++ ProcesarMensaje.py
@@ -1,9 +1,15 @@
-def ProcesamientoMensaje (mensaje):
+import string
+import re
+
+def ProcesamientoMensaje(mensaje):
     p = 0
     q = 1
-    Mensaje = mensaje.replace(" ","").upper()
+    Mensaje = mensaje.replace(" ","").upper() #Remove the spaces and convert the string to uppercase.
+    Mensaje = ''.join([i for i in Mensaje if not i.isdigit()]) #Remove digits from the message
+    pattern = r'[' + string.punctuation + ']'
+    Mensaje = re.sub(pattern, '', Mensaje) #Remove special characters from the message
 
-    #Eliminamos las letras con acentos, cambiamos a "v" por "u" y pasamos todo a mayusculas
+    #Remove the letters with accents, change "v" to "u" and "ñ" to "n". 
     Acentos = (
         ("á", "a"),
         ("é", "e"),
@@ -11,22 +17,24 @@ def ProcesamientoMensaje (mensaje):
         ("ó", "o"),
         ("ú", "u"),
         ("v","u"),
+        ("ñ","n"),
         ("Á","A"),
         ("É","E"),
         ("Í","I"),
         ("Ó","O"),
         ("Ú","U"),
-        ("V","U"))
+        ("V","U"),
+        ("Ñ","N"))
     for a,b in Acentos:
         Mensaje = Mensaje.replace(a,b)
     ListaMensaje = list(Mensaje)
     longitud = len(ListaMensaje)
 
-    #Si el mensaje solo es 1 letra se le agrega una "x" al final
+    #If the message is only 1 letter, an "x" is added at the end.
     if longitud == 1:
         ListaMensaje.append("X")
 
-    #Comprobamos si hay letras repetidas, si las hay se inserta una X entre las letras repetidas
+    #We check if there are repeated letters, if there are, we insert an X between the repeated letters.
     for k in ListaMensaje: 
         if ListaMensaje[p] == ListaMensaje [q]:
             ListaMensaje.insert(q,"X")
@@ -35,9 +43,13 @@ def ProcesamientoMensaje (mensaje):
         if   q >= longitud: 
             break
     
-    longitud = len(ListaMensaje) #Actualizamos la longitud de la lista
+    longitud = len(ListaMensaje) #Ppdate the length of the list.
 
-    if longitud % 2 != 0: #Comprobamos si el numero de letras en le mensaje es par, si no lo es se agrega una x al final
+    if longitud % 2 != 0: #We check if the number of letters in the message is even, if it is not, we add an x at the end.
         ListaMensaje.append("X")
 
     return ListaMensaje
+
+if __name__ == "__main__":
+    x = ProcesamientoMensaje("!!<31SalUDOS2 3al44 55MURNEy666<3132!&/(%$#(=????)(/&%$")
+    print(x)
